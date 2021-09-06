dc0302099ef7256d02f515090aa3eb797eff0ed4
AcharneAegis <paytonwaskew@gmail.com>
2021-09-06 13:57:13 -0500

    Add Input File and UpTo WS-Section

--- CS370Program1.cbl
+++ CS370Program1.cbl
@@ -37,4 +37,51 @@
       *        1.  Salary Total
       *    *************
       * CALCULATIONS
-      *    
+      *    TO BE FILLED IN LATER
+      *
+      ******************************************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. IBM-PC.
+       OBJECT-COMPUTER. IBM-PC.
+
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT EMPLOYEE-FILE
+               ASSIGN TO 'Knox.txt'
+               ORGANIZATION IS LINE SEQUENTIAL.
+           SELECT EMP-REPORT-FILE
+               ASSIGN TO PRINTER 'Knox-Salary-Report'.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       FD EMPLOYEE-FILE
+           RECORD CONTAINS 75 CHARACTERS.
+
+       01  EMPLOYEE-RECORD.
+           05  EMP-STORE-ID            PIC A(4).
+           05  EMP-ID                  PIC X(5).
+           05  EMP-POSITION            PIC A(2).
+           05  EMP-LAST-NAME           PIC X(10).
+           05  EMP-FIRST-NAME          PIC X(10).
+           05  EMP-STATUS              PIC X(1).
+           05  FILLER                  PIC 9(8).
+           05  FILLER                  PIC 9(8).
+           05  EMP-LAST-RAISE-DATE     PIC 9(8).
+           05  EMP-CURRENT-SALARY      PIC 9(8).
+
+       FD EMP-REPORT-FILE
+           RECORD CONTAINS 80 CHARACTERS.
+
+       01  REPORT-RECORD               PIC X(80).
+
+       WORKING-STORAGE SECTION.
+       
+       01  FLAGS-N-SWITCHES.
+           05  EOF-FLAG                PIC X           VALUE ' '.
+               88 NO-MORE-DATA                         VALUE 'N'.
+
+       01  DETAIL-FIELDS.
+           05  DF
+           
