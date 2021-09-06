c41bd98022b4e875a4864fa219612498fbf790bb
AcharneAegis <paytonwaskew@gmail.com>
2021-09-06 12:56:18 -0500

    GitHub Test

---
+++ CS370Program1.cbl
@@ -0,0 +1,40 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CS370PROGRAM1.
+       AUTHOR. P W ASKEW.
+      ******************************************************************
+      * This program serves to give practive with the basics of COBOL
+      * The problem given is a CEO needs us to write a report on the 
+      * employees that have not recieved a raise in the last year
+      * ******
+      * INPUT:
+      *    The PR1F21-Knox.txt file contains the following
+      *        1.  Store ID
+      *        2.  Employee ID
+      *        3.  Employee Position
+      *        4.  Employee Last Name
+      *        5.  Employee First Name
+      *        6.  Employee Middle Initial
+      *        7.  Hire Date
+      *        8.  Employee Status
+      *        9.  Separation Date
+      *        10.  Starting Yearly Salary
+      *        11.  Date of Last Pay Increase
+      *        12.  Current Yearly Salary
+      * *******
+      * OUTPUT:
+      *    The SALARY REPORT file contains the following
+      *    *************
+      *    DETAIL LINE:
+      *        1.  Employee ID
+      *        2.  Employee Position
+      *        3.  Employee First Name
+      *        4.  Employee Last Name
+      *        5.  Employee Status
+      *        6.  Date of Last Pay Increase
+      *        7.  Current Salary
+      *    **************
+      *    FINAL TOTALS
+      *        1.  Salary Total
+      *    *************
+      * CALCULATIONS
+      *    
