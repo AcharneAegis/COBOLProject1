9309584eca6b9674cf9c1f2c0a0d12eb7614c1ac
AcharneAegis <paytonwaskew@gmail.com>
2021-09-06 23:11:09 -0500

    Working on Base Function

--- CS370Program1.cbl
+++ CS370Program1.cbl
@@ -82,6 +82,153 @@
            05  EOF-FLAG                PIC X           VALUE ' '.
                88 NO-MORE-DATA                         VALUE 'N'.
 
-       01  DETAIL-FIELDS.
-           05  DF
+       01  TOTAL-FIELDS.
+           05  TF-SALARY-TOTAL         PIC S9(7)V99    VALUE +0.
+
+       01  REPORT-FIELDS.
+           05  PROPER-SPACING          PIC S9          VALUE +2.
+
+       01  CONSTANTS-FIELDS.
+           05  CF-CURRENT-DATE         PIC 9999/99/99.
+
+      *********************    OUTPUT AREA     *************************
+
+       01  HEADING-ONE.
+           05  H1-DATE                 PIC 9999/99/99. 
+           05                          PIC X(25)       VALUE SPACES.
+           05                          PIC A(13)       VALUE 
+                                                       'BENNET SHOES'.
+           05                          PIC A(19)       VALUE SPACES.
+           05                          PIC XXX         VALUE 'PWA'.
            
+       01  HEADING-TWO.
+           05                          PIC X(35)       VALUE SPACES.
+           05                          PIC X(15)       VALUE 
+                                                   'EMPLOYEE REPORT'.
+       01  HEADING-THREE.
+           05                          PIC X(36)       VALUE SPACES.
+           05                          PIC X(13)       VALUE 
+                                                       'KNOXVILLE, TN'.
+       01  HEADING-FOUR.
+           05                          PIC X(3)        VALUE SPACES.
+           05                          PIC X(3)        VALUE 'EMP'.
+           05                          PIC X(4)        VALUE SPACES.
+           05                          PIC X(3)        VALUE 'EMP'.
+           05                          PIC X(9)        VALUE SPACES.
+           05                          PIC X(3)        VALUE 'EMP'.
+           05                          PIC X(8)        VALUE SPACES.
+           05                          PIC X(3)        VALUE 'EMP'.
+           05                          PIC X(6)        VALUE SPACES.
+           05                          PIC X(4)        VALUE 'LAST'.
+           05                          PIC X(8)        VALUE 'CURRENT'.
+
+       01  HEADING-FIVE.
+           05                          PIC X(3)        VALUE SPACES.
+           05                          PIC X(2)        VALUE 'ID'.
+           05                          PIC X(5)        VALUE SPACES.
+           05                          PIC X(3)        VALUE 'POS'.
+           05                          PIC X(2)        VALUE SPACES.
+           05                          PIC X(10)       VALUE 
+                                                       'FIRST NAME'.
+           05                          PIC X(3)        VALUE SPACES.
+           05                          PIC X(9)        VALUE 
+                                                           'LAST NAME'.
+           05                          PIC X(3)        VALUE SPACES.
+           05                          PIC X(6)        VALUE 'STATUS'.
+           05                          PIC X(3)        VALUE SPACES.
+           05                          PIC X(8)        VALUE 'INCREASE'.
+
+
+       01  DETAIL-LINE.
+           05                          PIC X(2)        VALUE SPACES.
+           05  DL-EMP-ID               PIC X(5).
+           05                          PIC X(3)        VALUE SPACES.
+           05  DL-EMP-POS              PIC A(2).
+           05                          PIC X(3)        VALUE SPACES.
+           05  DL-EMP-FIRST-NAME       PIC X(10).
+           05                          PIC X(3)        VALUE SPACES.
+           05  DL-EMP-LAST-NAME        PIC X(10).
+           05                          PIC X(4)        VALUE SPACES.
+           05  DL-EMP-STATUS           PIC A(1).
+           05                          PIC X(5)        VALUE SPACES.
+           05  DL-EMP-LAST-RAISE-DATE  PIC 99/99/9999.
+           05                          PIC X(3)        VALUE SPACES.
+           05  DL-DOLLAR-SIGN          PIC X           VALUE '$'.
+           05  DL-EMP-CURRENT-SALARY   PIC S999999V99.
+
+
+       01  TOTAL-LINE.
+           05  FILLER                  PIC X(45)         VALUE SPACES.
+           05                          PIC X(13)       VALUE 
+                                                       'SALARY TOTAL:'.
+           05                          PIC X(2)        VALUE SPACES.
+           05  TL-DOLLAR-SIGN          PIC X           VALUE '$'.
+           05  TL-SALARY-TOTAL         PIC S9999999V99.
+
+       PROCEDURE DIVISION.
+       
+       10-CONTROL-MODULE.
+           
+           PERFORM 15-HSKPING-ROUTINE
+           PERFORM 25-PROCESS-INPUT-FILE
+           PERFORM 40-EOF-ROUTINE
+           .
+       15-HSKPING-ROUTINE.
+
+           OPEN INPUT EMPLOYEE-FILE
+               OUTPUT EMP-REPORT-FILE
+           ACCEPT H1-DATE FROM DATE YYYYMMDD
+           ACCEPT CF-CURRENT-DATE FROM DATE YYYYMMDD
+           PERFORM 20-HEADER-ROUTINE
+           .
+
+       20-HEADER-ROUTINE.
+           MOVE 1 TO PROPER-SPACING
+           WRITE REPORT-RECORD FROM HEADING-ONE
+               AFTER ADVANCING PAGE
+           MOVE 2 TO PROPER-SPACING
+           WRITE REPORT-RECORD FROM HEADING-TWO
+               AFTER ADVANCING PROPER-SPACING
+           WRITE REPORT-RECORD FROM HEADING-THREE
+               AFTER ADVANCING PROPER-SPACING
+           WRITE REPORT-RECORD FROM HEADING-FOUR
+               AFTER ADVANCING PROPER-SPACING
+           MOVE 1 TO PROPER-SPACING
+           WRITE REPORT-RECORD FROM HEADING-FIVE
+               AFTER ADVANCING PROPER-SPACING
+           MOVE 2 TO PROPER-SPACING
+           .
+       
+       25-PROCESS-INPUT-FILE.
+           PERFORM UNTIL NO-MORE-DATA
+               READ EMPLOYEE-FILE
+                   AT END
+                       MOVE 'N' TO EOF-FLAG
+                   NOT AT END 
+                       IF 
+                          
+                       ELSE
+                          
+                       END-IF
+                       PERFORM 30-VERIFY-EMPLOYEE-RAISE-DATE
+               END-READ
+           END-PERFORM
+           .
+       
+       PERFORM 30-VERIFY-EMPLOYEE-RAISE-DATE.
+           
+           MOVE EMP-ID TO DL-EMP-ID
+           MOVE EMP-POSITION TO DL-EMP-POS
+           MOVE EMP-FIRST-NAME TO DL-EMP-FIRST-NAME
+           MOVE EMP-LAST-NAME TO DL-EMP-LAST-NAME
+           MOVE EMP-STATUS TO DL-EMP-STATUS
+           MOVE EMP-LAST-RAISE-DATE TO DL-EMP-LAST-RAISE-DATE
+           MOVE EMP-CURRENT-SALARY TO DL-EMP-CURRENT-SALARY
+
+           MOVE DETAIL-LINE TO REPORT-RECORD
+           PERFORM 35-WRITE-A-LINE
+           MOVE 1 TO PROPER-SPACING
+
+           ADD EMP-CURRENT-SALARY TO TF-SALARY-TOTAL
+
+           .
